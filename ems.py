3293df40c6f68a8f1a15455b3b7c333d16f959de
Maleiyah <maleiyahdavis@yahoo.com>
2023-04-12 13:54:25 -0500

    add new_employee and update_employee functions

--- ems.py
+++ ems.py
@@ -2,15 +2,24 @@
 
 class ems():
 
-    def __init__(self, emp ):
-        self.emp = emp
+    def __init__(self):
+        self.emp = []
     
-    def add_New_Employee(self):
+    def add_New_Employee(self, employee):
+        for employee in self.emp: 
+            if employee in self:
+                print("This employee ID already exist, enter a valid ID: ")
+            else: 
+                self.emp.append(employee)
+
+    def update_Employee(self, attribute_name, new_value):
+        if hasattr(self, attribute_name):
+            setattr(self, attribute_name, new_value)
+        else:
+            print("Invalid attribute")
+
+    write_employee_list = csvwriter.write(self.emp)
         
-
-    def update_Employee(self, emp):
-    
-
     def remove_Employee(self, emp):
 
 
