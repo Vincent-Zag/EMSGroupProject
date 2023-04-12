7285399c903e97ef06c16b2a681852e1758a255f
Sadman Salam <sadman_65@yahoo.com>
2023-04-12 14:12:13 -0400

    updated main

--- MainEMS.py
+++ MainEMS.py
@@ -1,3 +1,6 @@
+import ems
+import employee
+import CSVWriter
 
 def main_menu():
     print("1. Add Employee")
@@ -8,22 +11,52 @@ def main_menu():
     choice = input("Enter your choice: ")
     return choice
 
-while True:
-    choice = main_menu()
-    if choice == '1':
-        firstName = input("Enter your first name:\n")
-        lastName = input("Enter your last name:\n")
-        salary = input("Enter salary for employee:\n")
-        dep = input("Which department?\n")
-    elif choice == '2':
-        update = input("Insert Employee ID:\n")
-    elif choice == '3':
-        remove = input("Enter Employee ID:\n")
-    elif choice == '4':
-        display = input("Enter Employee ID:\n")
-    elif choice == '5':
-        print("Goodbye!")
-        break
+def NameError(firstname, lastname):
+    if firstname.isalpha() or lastName.isalpha() == False:
+        print("Name has to have letters only, you cannot have special characters or numbers in your name")
+        return False
     else:
-        print("Invalid choice. Please try again.")
+        return True 
+    
+def InvSalary(salary):
+    if salary > 80000:
+        print("Employee max salary is 80000")
+    elif salary < 30000:
+        print("please dont be cheap minimum wage is 30000 here")
+    else:
+        print("salary has to be a integer")
 
+def InvOption(option):
+    if option > 5 or option < 1:
+        print("invalid option")
+
+if __name__=='__main__':
+    while True:
+        choice = main_menu()
+        try:
+            if choice == '1':
+                try:
+                    firstName = input("Enter first name:\n")
+                    lastName = input("Enter last name:\n")
+                    salary = input("Enter salary for employee:\n")
+                    dep = input("Which department?\n")
+
+                    if NameError(firstName, lastName):
+                        print("...name is A-Okay")
+
+                        #ems.addNewEmployee(firstName, lastName, salary, dep, employee.create_id())
+                except:
+                    print("error")
+            elif choice == '2':
+                update = input("Insert Employee ID:\n")
+            elif choice == '3':
+                remove = input("Enter Employee ID:\n")
+            elif choice == '4':
+                display = input("Enter Employee ID:\n")
+            elif choice == '5':
+                print("Goodbye!")
+                break
+            else:
+                print("Invalid choice. Please try again.")
+        except:
+            InvOption(choice)
