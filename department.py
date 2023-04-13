1e6adcd8607551029f354c7351ef88f99234483a
Vincent <60195945+Vincent-Zag@users.noreply.github.com>
2023-04-13 09:52:53 -0600

    Revert "Habib branch"

---
+++ department.py
@@ -0,0 +1,28 @@
+#Department Class
+import csvWriter
+
+class department():
+    def __init__(self, id, name, budget, phone):
+        self.id = id
+        self.name = name
+        self.budget = budget
+        self.phone = phone
+        self.department_list = []
+        
+    def add_department(self, id, name, budget, phone):
+        self.department_list.append(self.id,self.name,self.budget,self.phone)
+    
+    def remove_department(self, id):
+        #find in list then remove the info about department
+        pass
+
+    def update_department(self, attribute_name, new_value):
+        if hasattr(self, attribute_name):
+            setattr(self, attribute_name, new_value)
+        else:
+            print("Invalid attribute")
+
+    def get_list(self):
+
+    def list_depts(self, department_list):
+
