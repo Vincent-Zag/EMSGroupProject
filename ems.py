f52fefbe3541bd06a85e18be5d5a5dcacd262e65
Habib <hmulla@student.umgc.edu>
2023-04-13 10:29:32 -0400

    Added remove function

--- ems.py
+++ ems.py
@@ -3,7 +3,7 @@
 class ems():
 
     def __init__(self, emp ):
-        self.emp = emp
+        self.emp = []
     
     def add_New_Employee(self):
         
@@ -11,8 +11,20 @@ class ems():
     def update_Employee(self, emp):
     
 
-    def remove_Employee(self, emp):
+    def remove_Employee(self, id):
+        found_employee = None
+        for emp in self.emp:
+            if emp.id == id:
+                found_employee = emp
+            break
 
+    # If employee found, remove from list
+        if found_employee:
+            self.emp.remove(found_employee)
+  
+    # If employee not found, print error message
+        else:
+            print("Employee ID not found!!")
 
     def list_Emp_Info(self, emp):
 
